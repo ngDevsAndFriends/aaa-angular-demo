833ed4292d4cf924bba854f500f432069a2bb665
ng-Devs <contact@ngdevs.net>
2026-03-01 19:11:59 +0100

    added form validation in task-form.ts

--- src/app/task-form/task-form.ts
+++ src/app/task-form/task-form.ts
@@ -1,5 +1,5 @@
 import {Component, inject} from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {TaskService} from '../task-service';
 import {ActivatedRoute, Router} from '@angular/router';
 
@@ -17,8 +17,8 @@ export class TaskForm {
   private id = this.route.snapshot.paramMap.get("id");
 
   form = new FormGroup({
-    title: new FormControl(""),
-    description: new FormControl(""),
+    title: new FormControl("", [Validators.required]),
+    description: new FormControl("", [Validators.minLength(3)]),
   });
 
   constructor() {
@@ -28,8 +28,16 @@ export class TaskForm {
   }
 
   submit() {
-    this.taskService.addTask(this.form.value);
+    if(this.form.invalid) {
+      console.log("Your form is invalid. Please check the fields.");
+      console.log("Your form: ", this.form.value);
+      console.log("Title Errors: ", this.form.controls.title.errors);
+      console.log("Description Errors: ", this.form.controls.description.errors);
 
+      return;
+    }
+
+    this.taskService.addTask(this.form.value);
     this.router.navigate(["/"]);
   }
 }
