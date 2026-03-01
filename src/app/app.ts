08e3419a4c7865790322bf269534e2263a424843
ng-Devs <contact@ngdevs.net>
2026-03-01 17:41:20 +0100

    added router-outlet to app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import {TaskList} from './task-list/task-list';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    TaskList
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
