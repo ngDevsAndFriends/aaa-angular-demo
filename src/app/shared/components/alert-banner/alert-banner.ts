6cf463117d7bfd706bbb21a597fdb5aec9751727
ng-Devs <contact@ngdevs.net>
2026-03-01 21:33:39 +0100

    created the reusable alert-banner component

---
+++ src/app/shared/components/alert-banner/alert-banner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-banner',
+  imports: [],
+  templateUrl: './alert-banner.html',
+  styleUrl: './alert-banner.css',
+})
+export class AlertBanner {}
