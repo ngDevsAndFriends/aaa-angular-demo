11993fde975c5908774d4b045da719c9e4b49fa9
ng-Devs <contact@ngdevs.net>
2026-03-01 17:45:01 +0100

    added taskList and TaskForm routes in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {TaskList} from './task-list/task-list';
+import {TaskForm} from './task-form/task-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: TaskList },
+  { path: "add-task", component: TaskForm }
+];
